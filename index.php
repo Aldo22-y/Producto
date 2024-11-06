ded6a51e435cd993717520dfca588d3bc547c38b
Aldo22-y <jinmehuaytan16@gmail.com>
2024-11-06 12:54:17 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Gestión de Restaurante</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Bienvenido a mi Sistema de Gestión de Restaurante</h1>
+
+        <h2>Opciones Disponibles</h2>
+        <ul>
+            <center><a href="clientes/index.php" class="button">Gestión de Clientes</a></center> <br><br>
+            <center><a href="productos/index.php" class="button">Gestión de Productos</a></center>
+        </ul>
+    </div>
+</body>
+</html>
